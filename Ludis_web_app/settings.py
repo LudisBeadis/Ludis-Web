3d0d29f30a01c9458f7591719170f266294548f2
LudisBeadis <83122363+LudisBeadis@users.noreply.github.com>
2021-09-08 09:45:21 +0300

    Update settings.py

--- Ludis_web_app/settings.py
+++ Ludis_web_app/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,3 +106,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
+django_heroku.settings(locals())
